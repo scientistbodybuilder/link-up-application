de85ed612681b83b090b1524ba51b52fe4176b6e
scientistbodybuilder <o.jikineh@mail.utoronto.ca>
2024-06-05 16:19:20 -0400

    Redesigned the home page/added scroll feature, added javascript for icon functionality, create SQL database (not connected yet)

--- app.py
+++ app.py
@@ -2,9 +2,61 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+ORGS = [
+    {
+        'id': 1,
+        'name': 'Google',
+        'cards': 3,
+        'reviews gained': 50
+    },
+    {
+        'id':2,
+        'name': 'Netflix',
+        'cards': 5,
+        'reviews gained': 72
+    },
+    {
+        'id': 3,
+        'name': 'Indigo',
+        'cards': 3,
+        'reviews gained': 60
+    }
+]
+
 @app.route("/")
-def hello_world():
-    return render_template('home.html')
+def home_page():
+    return render_template('home.html', orgs=ORGS)
+
+@app.route("/contact")
+def contact_page():
+    return render_template('contact.html')
+
+@app.route("/login")
+def login_page():
+    return render_template('login.html')
+
+@app.route("/signup")
+def signup_page():
+    return render_template('signup.html')
+
+
+
+#{% for org in orgs %}
+#            <div>
+#                <h4>{{orgs['name']}}</h4>
+#                <div>
+#                    Activated Cards: {{orgs['cards']}}
+#                    <br>
+#                    <b>Reviews Added: {{orgs['reviews gained']}}</b>
+#                </div>
+#                
+#            </div>
+#        {% endfor %}
+
+
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
---
+++ templates/javascript/home.js
@@ -0,0 +1,13 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const tags = document.querySelectorAll('.drop-down .button')
+    const toggleBtn = document.querySelector('.toggle_btn')
+    const toggleBtnIcon = document.querySelector('.toggle_btn i')
+    const dropDownMenu = document.querySelector('.dropdown_menu')
+
+    toggleBtnIcon.addEventListener('click', () => {
+        dropDownMenu.classList.toggle('open')
+        console.log('icon clicked')
+    })
+})
+
