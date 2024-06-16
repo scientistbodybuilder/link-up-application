9b9be7259a38785d7635c2b727f5f8d2093b718a
scientistbodybuilder <o.jikineh@mail.utoronto.ca>
2024-06-15 23:17:39 -0400

    Reorganized html, css, and javascript files into different template and static folders. Connected app.py to a local MySQL database

--- app.py
+++ app.py
@@ -1,32 +1,23 @@
 from flask import Flask, render_template
+from flask_mysqldb import MySQL
+from dotenv import load_dotenv
+import os
+from flask_login import UserMixin
 
 app = Flask(__name__)
 
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
-ORGS = [
-    {
-        'id': 1,
-        'name': 'Google',
-        'cards': 3,
-        'reviews gained': 50
-    },
-    {
-        'id':2,
-        'name': 'Netflix',
-        'cards': 5,
-        'reviews gained': 72
-    },
-    {
-        'id': 3,
-        'name': 'Indigo',
-        'cards': 3,
-        'reviews gained': 60
-    }
-]
+
+mysql = MySQL(app)
 
 @app.route("/")
 def home_page():
-    return render_template('home.html', orgs=ORGS)
+    return render_template('home.html')
 
 @app.route("/contact")
 def contact_page():
@@ -39,20 +30,6 @@ def login_page():
 @app.route("/signup")
 def signup_page():
     return render_template('signup.html')
-
-
-
-#{% for org in orgs %}
-#            <div>
-#                <h4>{{orgs['name']}}</h4>
-#                <div>
-#                    Activated Cards: {{orgs['cards']}}
-#                    <br>
-#                    <b>Reviews Added: {{orgs['reviews gained']}}</b>
-#                </div>
-#                
-#            </div>
-#        {% endfor %}
 
 
 
